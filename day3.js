02eb5219cf784c87ddb08755870aa7424625e161
KartikeyKakaria <kartikeykakaria2@gmail.com>
2022-12-04 12:15:55 +0530

    day3 part2

--- day3.js
+++ day3.js
@@ -9,8 +9,8 @@ function getAlphaVal(alphabet) {
 function removeDuplicates(str) {
     return Array.from(new Set(Array.from(str))).toString();
 }
-let priorities = 0;
-data.forEach(rucksack => {
+
+function part1(rucksack) {
     const compartment1 = removeDuplicates(rucksack.slice(0, rucksack.length / 2));
     const compartment2 = removeDuplicates(rucksack.slice(rucksack.length / 2));
     for (i in compartment1) {
@@ -20,5 +20,25 @@ data.forEach(rucksack => {
             priorities += getAlphaVal(char)
         }
     }
+}
+
+function part2(rucksack, index) {
+    if (index % 3 == 0) {
+        const rucksack1 = removeDuplicates(rucksack)
+        const rucksack2 = removeDuplicates(data[index + 1])
+        const rucksack3 = removeDuplicates(data[index + 2])
+        for (i in rucksack1) {
+            const char = rucksack1[i];
+            const exists = rucksack2.indexOf(char) !== -1 && rucksack3.indexOf(char) !== -1 && char !== "," && char !== "\r";
+            if (exists) {
+                priorities += getAlphaVal(char)
+            }
+        }
+    }
+}
+let priorities = 0;
+data.forEach((rucksack, index) => {
+    // part1(rucksack)
+    part2(rucksack, index)
 })
 console.log(priorities)
